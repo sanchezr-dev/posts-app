a7ef9a198735585ccf0d0e62dd70d8d7d33ec153
= <=>
2023-09-04 17:52:54 -0400

    Module federation changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require("@module-federation/nextjs-mf")
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "green-app",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./home": "./src/pages/index.tsx",
+        },
+        remotes: {},
+        shared: {},
+        extraOptions: {
+          exposePages: true,
+        },
+      }),
+    )
+
+    return config
+  },
 }
 
 module.exports = nextConfig
